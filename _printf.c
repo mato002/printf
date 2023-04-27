9d0303f642fcabbb00c3c195111a8b0fc2c69c9a
mato002 <mathiasodhis@gmail.com>
2023-04-26 21:45:15 -0700

    updates

--- _printf.c
+++ _printf.c
@@ -1,28 +1,22 @@
 #include "main.h"
 
-
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Producs ooutput according to a format
- * @format: character string
- * Return: Number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
-
 int _printf(const char *format, ...)
 {
-	int i;
-	int printed = 0;
-	int printed_chars = 0;
-	int flags, width, precision, size;
-	int buff_ind = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFFER];
+	char buffer[BUFF_SIZE];
+
 	if (format == NULL)
-	{
 		return (-1);
-	}
+
 	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
@@ -30,14 +24,13 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFFER)
-			{
+			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-				printed_chars++;
-			}
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
-			{
+		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
@@ -45,30 +38,30 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
-			{
 				return (-1);
-				printed_chars += printed;
-			}
+			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
+
 	va_end(list);
+
 	return (printed_chars);
 }
 
 /**
- * print_buffer - Prints buffer content if it exists
- * @buffer: Array of characters
- * @buff_ind: index at which to add next character and reprecents length
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-	{
 		write(1, &buffer[0], *buff_ind);
-	}
+
 	*buff_ind = 0;
 }
+
